b7601a6474d5f5316d2a4a9e2bcc7bcd370fff02
Isaac Yuri <isaacyuri22@gmail.com>
2025-08-24 09:08:12 -0300

    :sparkles: feat: define the entity Task with basic attributes

--- src/main/java/com/isaac/todolist_backend/model/Task.java
+++ src/main/java/com/isaac/todolist_backend/model/Task.java
@@ -1,1 +1,19 @@
+package com.isaac.todolist_backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+}
